ed4ddb484d4e1816f0b58a8818cfe1ae1346f217
treyburn <travisreyburn@gmail.com>
2022-07-14 16:55:37 -0600

    created server implementation

---
+++ cmd/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"path/filepath"
+
+	"github.com/treyburn/boggle/api"
+	"github.com/treyburn/boggle/repository"
+	"github.com/treyburn/boggle/rpc"
+	"github.com/treyburn/boggle/solver"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+const port = 50051
+
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalln("err creating logger: ", err)
+	}
+	dictionary, err := filepath.Abs("./assets/3_letter_dictionary.txt")
+	if err != nil {
+		logger.Error("creating filepath", zap.Error(err))
+	}
+	repo := repository.NewInMemory()
+	sol := solver.NewOffTheShelfSolver(repo, logger, dictionary)
+
+	service := rpc.NewBoggleService(repo, sol, logger)
+	server := grpc.NewServer()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
+	if err != nil {
+		logger.Error("creating listener", zap.Error(err))
+	}
+
+	api.RegisterBoggleServiceServer(server, service)
+
+	logger.Info(fmt.Sprintf("serving on port %v", port))
+
+	if err := server.Serve(listener); err != nil {
+		logger.Fatal("server shutting down", zap.Error(err))
+	}
+
+}
